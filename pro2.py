2aae9157a83e472390eb2ee8157cdce0a226fd49
BEEMRAO <52704334+BEEMRAO@users.noreply.github.com>
2019-07-16 10:17:18 +0530

    Create pro2.py

---
+++ pro2.py
@@ -0,0 +1,13 @@
+x,y=input().strip().split(" ")
+y=int(x)
+z=0
+while z<len(x)-1 and y:
+  if(x[z]>x[z+1]):
+    y-=1
+    x=x[:z]+x[z+1:]
+    if(z!=0):
+            z-=1
+  else:
+    z+=1
+m=x[:len(x)-y]
+print(m)
